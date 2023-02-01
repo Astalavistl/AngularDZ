3531045b3c33e1d8b24e43245bbff7739d2bed01
Astalavistl <123003834+Astalavistl@users.noreply.github.com>
2023-02-01 18:27:42 +0000

    hobby

--- src/app/hobby/hobby.component.ts
+++ src/app/hobby/hobby.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './hobby.component.html',
   styleUrls: ['./hobby.component.css']
 })
-export class HobbyComponent {
 
+export class HobbyComponent { 
+  title = "XZ"
+  time = Date.now()
+
+  imgs = [
+    "/assets/d.jpg",
+    "/assets/kr.jpg",
+    "/assets/kr2.jpeg"
+  ]
+
+  
+  imgClick = (event: Event) => {
+    console.log((event.target as HTMLImageElement).getAttribute('src')); 
+    console.log("clicked@" + (event.target as HTMLImageElement).src);
+  }
+ 
 }
